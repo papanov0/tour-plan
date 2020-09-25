0b3a4358372300e12ec492d18eb1c57ed3fa6456
papanov0 <papanov0@yandex.ru>
2020-09-25 10:10:05 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-var mySwiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.hotel-slider__button--next',
   },
   keyboard: {
     enabled: true,
   },
-})
+});
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.reviews-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
+  },
+  keyboard: {
+    enabled: true,
+  },
+});
+
