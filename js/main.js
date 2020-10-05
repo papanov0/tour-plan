53c521b787beb9f3a7d03a5bd632eb594b777edf
papanov0 <papanov0@yandex.ru>
2020-10-05 16:12:03 +0300

    create: added modal window & responsive

--- js/main.js
+++ js/main.js
@@ -1,33 +1,55 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      prevEl: '.hotel-slider__button--prev',
+      nextEl: '.hotel-slider__button--next',
+    },
+    keyboard: {
+      enabled: true,
+    },
+  });
+  
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      prevEl: '.reviews-slider__button--prev',
+      nextEl: '.reviews-slider__button--next',
+    },
+    keyboard: {
+      enabled: true,
+    },
+  });
+  
+  let menuButton = $('.menu-button');
+  menuButton.on('click', function () {
+    $('.navbar-button').toggleClass('navbar-button--visible');
+  });
 
-  // Navigation arrows
-  navigation: {
-    prevEl: '.hotel-slider__button--prev',
-    nextEl: '.hotel-slider__button--next',
-  },
-  keyboard: {
-    enabled: true,
-  },
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__clouse");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
+  
 });
-
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    prevEl: '.reviews-slider__button--prev',
-    nextEl: '.reviews-slider__button--next',
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
-
-let menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  document.querySelector('.navbar-button').classList.toggle('navbar-button--visible');
-});
-
